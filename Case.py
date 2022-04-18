755258e64310a0bb11fe1929c14c62c91470cc72
Matthew Van Ausdal <msvanaus@purdue.edu>
2022-04-17 20:02:59 -0400

    Beginner PLL (bpll) data

---
+++ Case.py
@@ -0,0 +1,5 @@
+import json
+def parseCases(path):
+    
+class Case:
+    def __init__(self, path):
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import sys
 import random
 
 
+def bpll():
+
+
 def scramble(numScrambles=1):
     moves = ['F', 'B', 'U', 'D', 'R', 'L']
     modifiers = [' ', '\' ', '2 ']
     random.seed()
     lastMove = 7
-    currentMove = 0
     for i in range(numScrambles):
         moveList = ''
         for j in range(20):
@@ -31,7 +33,11 @@ if __name__ == "__main__":
     for i, arg in enumerate(sys.argv):
         # print(f"Arg: {arg}, Next: {sys.argv[i+1] if (i < len(sys.argv)-1) else None}")
         if arg == '-s':
+            # Shows a random scramble algorithm for a solved cube. If the next argument is an integer, it shows that
+            # many cube scrambles
             if i < len(sys.argv) - 1 and sys.argv[i+1].isdigit():
                 scramble(int(sys.argv[i+1]))
             else:
                 scramble()
+        if arg == '-bpll':
+
